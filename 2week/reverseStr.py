178b25fd28595a899f45e36b3fbb189441fdb3de
DahyeonKang <69884205+DahyeonKang@users.noreply.github.com>
2021-10-03 00:23:11 +0900

    #2week

---
+++ 2week/reverseStr.py
@@ -0,0 +1,18 @@
+'''
+* 프로그램명 : reverseStr.py
+* 작성자 : 2019015031 강다현
+* 작성일 : 2021.09.15
+*프로그램 설명 : 문자열의 내용을 역으로 바꾸는 순환함수 구현하기
+'''
+
+def reverse(str):
+    n = len(str)
+    if n == 1:
+        return str
+    return str[-1] + reverse(str[0:n-1])
+
+str = 'When in Rome, do as the Romans do.'
+
+print()
+print("원문장: ", str)
+print("역문장: ", reverse(str))
