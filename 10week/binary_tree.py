f674d153ca115fb0ed8aa64e9b98e68034797e4f
DahyeonKang <kl1223k@naver.com>
2021-11-11 01:07:45 +0900

    # 10week
    fix directory, module

--- 10week/binary_tree.py
+++ 10week/binary_tree.py
@@ -5,53 +5,10 @@
 구현한 프로그램 작성하기
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-# import sys, os
-# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-# from 6week import circularQueue.CircularQueue
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__)))))
 
-
-MAX_QSIZE = 10  # 원형 큐의 크기
-
-
-class CircularQueue:
-    def __init__(self):
-        self.front = 0  # 큐의 front(전단) 위치
-        self.rear = 0  # 큐의 rear(후단) 위치
-        self.items = [None] * MAX_QSIZE  # 항목 저장용 리스트
-
-    def isEmpty(self):  # 공백상태
-        return self.front == self.rear
-
-    def isFull(self):  # 포화상태
-        return self.front == (self.rear + 1) % MAX_QSIZE
-
-    def clear(self):
-        self.front = self.rear
-
-    def enqueue(self, item):
-        if not self.isFull():  # 포화상태가 아니면
-            self.rear = (self.rear + 1) % MAX_QSIZE  # rear 회전
-            self.items[self.rear] = item  # rear 위치에 삽입
-
-    def dequeue(self):
-        if not self.isEmpty():  # 공백상태가 아니면
-            self.front = (self.front + 1) % MAX_QSIZE  # front 회전
-            return self.items[self.front]  # front 위치의 항목 반환
-
-    def peek(self):
-        if not self.isEmpty():
-            return self.items[(self.front + 1) % MAX_QSIZE]
-
-    def size(self):
-        return (self.rear - self.front + MAX_QSIZE) % MAX_QSIZE
-
-    def display(self):
-        out = []
-        if self.front < self.rear:
-            out = self.items[self.front+1:self.rear+1]  # 슬라이싱
-        else:
-            out = self.items[self.front+1:MAX_QSIZE] + self.items[0:self.rear+1]  # 슬라이싱
-        print("[f=%s, r=%d] ==> " % (self.front, self.rear), out)
+from week06.circularQueue import CircularQueue
 
 
 class TNode:
