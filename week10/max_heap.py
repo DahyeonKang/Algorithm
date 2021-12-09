950bcd5865242abee07ef90e2d62e4f3ae79ee5c
DahyeonKang <kl1223k@naver.com>
2021-12-09 18:31:36 +0900

    # 10week

--- week10/max_heap.py
+++ week10/max_heap.py
@@ -56,15 +56,16 @@ class MaxHeap:
             return hroot
 
 
-heap = MaxHeap()
-data = [2, 5, 4, 8, 9, 3, 7, 3]
-print('[삽입 연산] : ' + str(data))
-
-for elem in data:
-    heap.insert(elem)
-
-heap.display('[ 삽입 후 ]: ')
-heap.delete()
-heap.display('[ 삭제 후 ]: ')
-heap.delete()
-heap.display('[ 삭제 후 ]: ')
+if __name__ == "__main__":
+    heap = MaxHeap()
+    data = [2, 5, 4, 8, 9, 3, 7, 3]
+    print('[삽입 연산]: ' + str(data))
+    
+    for elem in data:
+        heap.insert(elem)
+    
+    heap.display('[삽입 후 ]: ')
+    heap.delete()
+    heap.display('[삭제 후 ]: ')
+    heap.delete()
+    heap.display('[삭제 후 ]: ')
